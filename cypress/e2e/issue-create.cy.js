abb719b57723f2658fc6056e3a41f79a7bd4d6b1
Jauues <167766521+Jauues@users.noreply.github.com>
2024-05-26 01:32:34 +0300

    Assignment 2

--- cypress/e2e/issue-create.cy.js
+++ cypress/e2e/issue-create.cy.js
@@ -1,89 +1,177 @@
-describe('Issue create', () => {
+import { faker } from "@faker-js/faker";
+
+const createIssueModal = '[data-testid="modal:issue-create"]';
+const titleInput = 'input[name="title"]';
+const descriptionInput = ".ql-editor";
+const issueType = '[data-testid="select:type"]';
+const priority = '[data-testid="select:priority"]';
+const reporter = '[data-testid="select:reporterId"]';
+const assignee = '[data-testid="form-field:userIds"]';
+const submitButton = 'button[type="submit"]';
+const issueTypeBug = '[data-testid="select-option:Bug"]';
+const issueIconBug = '[data-testid="icon:bug"]';
+const reporterNamePickleRick = '[data-testid="select-option:Pickle Rick"]';
+const assigneeNameLordGaben = '[data-testid="select-option:Lord Gaben"]';
+const priorityHighest = '[data-testid="select-option:Highest"]';
+const backlog = '[data-testid="board-list:backlog"]';
+const listIssue = '[data-testid="list-issue"]';
+const divBacklogAssigneeAvatar = ".sc-kIPQKe";
+const avatarLordGaben = '[data-testid="avatar:Lord Gaben"]';
+const issueIconTask = '[data-testid="icon:task"]';
+const priorityLow = '[data-testid="select-option:Low"]';
+const reporterNameBabyYoda = '[data-testid="select-option:Baby Yoda"]';
+const priorityIconHighest = '[data-testid="icon:arrow-up"]';
+const priorityColorHighest = "rgb(205, 19, 23)";
+const priorityIconLow = '[data-testid="icon:arrow-down"]';
+const priorityColorLow = "rgb(45, 135, 56)";
+
+describe("Creating issues", () => {
   beforeEach(() => {
-    cy.visit('/');
+    cy.visit("/");
     cy.url()
-      .should('eq', `${Cypress.env('baseUrl')}project/board`)
+      .should("eq", `${Cypress.env("baseUrl")}project/board`)
       .then((url) => {
-        // System will already open issue creating modal in beforeEach block
-        cy.visit(url + '/board?modal-issue-create=true');
+        cy.visit(url + "/board?modal-issue-create=true");
       });
   });
 
-  it('Should create an issue and validate it successfully', () => {
-    // System finds modal for creating issue and does next steps inside of it
-    cy.get('[data-testid="modal:issue-create"]').within(() => {
-      // Type value to description input field
-      cy.get('.ql-editor').type('TEST_DESCRIPTION');
-      cy.get('.ql-editor').should('have.text', 'TEST_DESCRIPTION');
-
-      // Type value to title input field
-      // Order of filling in the fields is first description, then title on purpose
-      // Otherwise filling title first sometimes doesn't work due to web page implementation
-      cy.get('input[name="title"]').type('TEST_TITLE');
-      cy.get('input[name="title"]').should('have.value', 'TEST_TITLE');
-
-      // Open issue type dropdown and choose Story
-      cy.get('[data-testid="select:type"]').click();
-      cy.get('[data-testid="select-option:Story"]').wait(1000).trigger('mouseover').trigger('click');
-      cy.get('[data-testid="icon:story"]').should('be.visible');
-
-      // Select Baby Yoda from reporter dropdown
-      cy.get('[data-testid="select:reporterId"]').click();
-      cy.get('[data-testid="select-option:Baby Yoda"]').click();
-
-      // Select Baby Yoda from assignee dropdown
-      cy.get('[data-testid="form-field:userIds"]').click();
-      cy.get('[data-testid="select-option:Pickle Rick"]').click();
-
-      // Click on button "Create issue"
-      cy.get('button[type="submit"]').click();
-    });
-
-    // Assert that modal window is closed and successful message is visible
-    cy.get('[data-testid="modal:issue-create"]').should('not.exist');
-    cy.contains('Issue has been successfully created.').should('be.visible');
-
-    // Reload the page to be able to see recently created issue
-    // Assert that successful message has dissappeared after the reload
-    cy.reload();
-    cy.contains('Issue has been successfully created.').should('not.exist');
-
-    // Assert than only one list with name Backlog is visible and do steps inside of it
-    cy.get('[data-testid="board-list:backlog"]')
-      .should('be.visible')
-      .and('have.length', '1')
-      .within(() => {
-        // Assert that this list contains 5 issues and first element with tag p has specified text
-        cy.get('[data-testid="list-issue"]')
-          .should('have.length', '5')
-          .first()
-          .find('p')
-          .contains('TEST_TITLE')
-          .siblings()
-          .within(() => {
-            //Assert that correct avatar and type icon are visible
-            cy.get('[data-testid="avatar:Pickle Rick"]').should('be.visible');
-            cy.get('[data-testid="icon:story"]').should('be.visible');
-          });
-      });
-
-    cy.get('[data-testid="board-list:backlog"]')
-      .contains('TEST_TITLE')
-      .within(() => {
-        // Assert that correct avatar and type icon are visible
-        cy.get('[data-testid="avatar:Pickle Rick"]').should('be.visible');
-        cy.get('[data-testid="icon:story"]').should('be.visible');
-      });
+  it("Should create an issue & assert that issue is visible on the board", () => {
+    let description = "My bug description";
+    let title = "Bug";
+    createNewIssue(
+      description,
+      title,
+      issueTypeBug,
+      issueIconBug,
+      priorityHighest,
+      reporterNamePickleRick,
+      assigneeNameLordGaben
+    );
+    verifyNewIssueOnBacklog(
+      title,
+      issueIconBug,
+      avatarLordGaben,
+      priorityIconHighest,
+      priorityColorHighest
+    );
   });
 
-  it('Should validate title is required field if missing', () => {
-    // System finds modal for creating issue and does next steps inside of it
-    cy.get('[data-testid="modal:issue-create"]').within(() => {
-      // Try to click create issue button without filling any data
-      cy.get('button[type="submit"]').click();
-
-      // Assert that correct error message is visible
-      cy.get('[data-testid="form-field:title"]').should('contain', 'This field is required');
-    });
+  it("Should create an issue with random data & assert that issue is visible on the board", () => {
+    let description = faker.lorem.words(5);
+    let title = faker.lorem.words(1);
+    createTaskNoAssignee(description, title, priorityLow, reporterNameBabyYoda);
+    verifyNewIssueOnBacklogNoAssignee(
+      title,
+      issueIconTask,
+      priorityIconLow,
+      priorityColorLow
+    );
   });
 });
+
+function createNewIssue(
+  myDescription,
+  myTitle,
+  myIssueType,
+  myIssueIcon,
+  myPriority,
+  myReporter,
+  myAssignee
+) {
+  cy.get(createIssueModal).within(() => {
+    cy.get(descriptionInput)
+      .type(myDescription)
+      .should("have.text", myDescription);
+    cy.get(titleInput).type(myTitle).should("have.value", myTitle);
+    cy.get(issueType).click();
+    cy.get(myIssueType).wait(1000).trigger("mouseover").trigger("click");
+    cy.get(myIssueIcon).should("be.visible");
+    cy.get(priority).click();
+    cy.get(myPriority).wait(1000).trigger("mouseover").trigger("click");
+    cy.get(reporter).click();
+    cy.get(myReporter).click();
+    cy.get(assignee).click();
+    cy.get(myAssignee).click();
+    cy.get(submitButton).click();
+  });
+}
+
+function createTaskNoAssignee(
+  myDescription,
+  myTitle,
+  myPriorityLevel,
+  myReporterName
+) {
+  cy.get(createIssueModal).within(() => {
+    cy.get(descriptionInput).type(myDescription);
+    cy.get(titleInput).type(myTitle);
+    cy.get(issueType).first().find("i").siblings().contains("Task");
+    cy.get(issueIconTask).scrollIntoView().should("be.visible");
+    cy.get(priority).click();
+    cy.get(myPriorityLevel).wait(1000).trigger("mouseover").trigger("click");
+    cy.get(reporter).click();
+    cy.get(myReporterName).click();
+    cy.get(submitButton).click();
+  });
+}
+
+function verifyNewIssueOnBacklog(
+  prevIssueTitle,
+  prevIssueTypeIcon,
+  prevAssigneeAvatar,
+  prevPriorityIcon,
+  priorityLevelColor
+) {
+  cy.get(createIssueModal).should("not.exist");
+  cy.contains("Issue has been successfully created.").should("be.visible");
+  cy.reload();
+  cy.contains("Issue has been successfully created.").should("not.exist");
+  cy.get(backlog)
+    .should("be.visible")
+    .and("have.length", "1")
+    .within(() => {
+      cy.get(listIssue)
+        .should("have.length", "5")
+        .first()
+        .find("p")
+        .contains(prevIssueTitle)
+        .siblings()
+        .within(() => {
+          cy.get(prevIssueTypeIcon).should("be.visible");
+          cy.get(prevAssigneeAvatar).should("be.visible");
+          cy.get(prevPriorityIcon)
+            .should("be.visible")
+            .and("have.css", "color", priorityLevelColor);
+        });
+    });
+}
+
+function verifyNewIssueOnBacklogNoAssignee(
+  prevIssueTitle,
+  prevIssueTypeIcon,
+  prevPriorityIcon,
+  priorityLevelColor
+) {
+  cy.get(createIssueModal).should("not.exist");
+  cy.contains("Issue has been successfully created.").should("be.visible");
+  cy.reload();
+  cy.contains("Issue has been successfully created.").should("not.exist");
+  cy.get(backlog)
+    .should("be.visible")
+    .and("have.length", "1")
+    .within(() => {
+      cy.get(listIssue)
+        .should("have.length", "5")
+        .first()
+        .find("p")
+        .contains(prevIssueTitle)
+        .siblings()
+        .within(() => {
+          cy.get(prevIssueTypeIcon).should("be.visible");
+          cy.get(divBacklogAssigneeAvatar).should("not.be.visible");
+          cy.get(prevPriorityIcon)
+            .should("be.visible")
+            .and("have.css", "color", priorityLevelColor);
+        });
+    });
+}
